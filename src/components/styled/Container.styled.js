0bfa71506e9c5c215050d33215c4083bde834b7e
Dipansh Parmar <dipanshparmar870@gmail.com>
2022-09-10 14:37:28 +0530

    added new props

--- src/components/styled/Container.styled.js
+++ src/components/styled/Container.styled.js
@@ -7,6 +7,14 @@ const Container = styled.div`
   color: ${props => props.color};
   border: ${props => props.border || 0};
   border-radius: ${props => props.radius};
+  height: ${props => props.height};
+  width: ${props => props.width};
+  background-image: ${props => props.bgImg && `url('${props.bgImg}')`};
+  background-size: ${props => props.bgImg && 'cover'};
+  background-position: ${props => props.bgImg && 'center'};
+  display: ${props => props.center && 'flex'};
+  justify-content: ${props => props.center && 'center'};
+  align-items: ${props => props.center && 'center'};
 `
 
 export default Container
