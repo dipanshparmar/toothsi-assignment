db573126ed6807bba6aa54f9319758512bb31963
Dipansh Parmar <dipanshparmar870@gmail.com>
2022-09-10 13:47:31 +0530

    added alignment

--- src/components/styled/Text.styled.js
+++ src/components/styled/Text.styled.js
@@ -4,6 +4,7 @@ const Text = styled.p`
   font-size: ${props => props.size};
   font-weight: ${props => props.weight};
   color: ${props => props.color};
+  text-align: ${props => props.align};
 `
 
 export default Text
